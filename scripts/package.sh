984ca1b013076017f8380b470717700549998dde
bipinupd <bipinupd@gmail.com>
2020-09-14 11:18:59 -0400

    Additional build files

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
-mkdir -p /workspace/dlp/{deIdentify_templates,inspect_templates,scripts}
-mkdir -p /workspace/dlp/tests/utils
-mkdir -p /workspace/dlp/tests/it-test
-cd /workspace/dlp
-find . -type f -name '*.*' -delete
-cd /workspace/datalake-repo/
-cp -R dlp/scripts/ /workspace/dlp/scripts/
-cp -R dlp/tests/ /workspace/dlp/tests/
-cp -R dlp/cloudbuild* /workspace/dlp/
+mkdir -p /workspace/package/DLPTemplates/{deIdentify_templates,inspect_templates,scripts}
+mkdir -p /workspace/package/DLPTemplates/tests/utils
+mkdir -p /workspace/package/DLPTemplates/tests/it-test
+cd /workspace/DLPTemplates/
+cp -R scripts/ /workspace/package/DLPTemplates/scripts/
+cp -R tests/ /workspace/package/DLPTemplates/tests/
+cp -R cloudbuild* /workspace/package/DLPTemplates/dlp/
 input="/workspace/dlp-diff.txt"
 while IFS= read -r line
 do
   subfolder=$(echo "$line" | cut -d '/' -f 2)
   if [[ "$subfolder" == "deIdentify_templates" ]]; then
-    cp "$line" "/workspace/dlp/deIdentify_templates/"
+    cp "$line" "/workspace/package/DLPTemplates/deIdentify_templates/"
     template_file=`echo "${line}" | awk -F/ '{print $NF}'`
     it_test_file="${template_file%.*}"_test.py
-    cp "dlp/tests/it-test/$it_test_file" "/workspace/dlp/tests/it-test/"
+    cp "tests/it-test/$it_test_file" "/workspace/package/DLPTemplates/tests/it-test/"
   fi
   if [[ "$subfolder" == "inspect_templates" ]]; then
-    cp "$line" "/workspace/dlp/inspect_templates/"
+    cp "$line" "/workspace/package/DLPTemplates/inspect_templates/"
     template_file=`echo "${line}" | awk -F/ '{print $NF}'`
     it_test_file="${template_file%.*}"_test.py
-    cp "dlp/tests/it-test/$it_test_file" "/workspace/dlp/tests/it-test/"
+    cp "tests/it-test/$it_test_file" "/workspace/package/DLPTemplates/tests/it-test/"
   fi
 done < "$input"
-cd /workspace
-zip -r dlp.zip dlp/
+cd /workspace/package
+zip -r DLPTemplates.zip DLPTemplates/
