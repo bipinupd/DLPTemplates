8a51f423846642f167464ad043eb25039063537a
bipinupd <bipinupd@gmail.com>
2020-09-14 15:13:02 -0400

    package

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,17 +7,20 @@ cp -R scripts/ /workspace/package/DLPTemplates/scripts/
 cp tests/* /workspace/package/DLPTemplates/tests/
 cp cloudbuild* /workspace/package/DLPTemplates/
 input="/workspace/dlp-diff.txt"
+cat /workspace/dlp-diff.txt
 cd /workspace/DLPTemplates
 while IFS= read -r line
 do
   subfolder=$(echo "$line" | cut -d '/' -f 2)
   if [[ "$subfolder" == "deIdentify_templates" ]]; then
+    echo "$line"
     cp "$line" "/workspace/package/DLPTemplates/deIdentify_templates/"
     template_file=`echo "${line}" | awk -F/ '{print $NF}'`
     it_test_file="${template_file%.*}"_test.py
     cp "tests/it-test/$it_test_file" "/workspace/package/DLPTemplates/tests/it-test/"
   fi
   if [[ "$subfolder" == "inspect_templates" ]]; then
+    echo "$line"
     cp "$line" "/workspace/package/DLPTemplates/inspect_templates/"
     template_file=`echo "${line}" | awk -F/ '{print $NF}'`
     it_test_file="${template_file%.*}"_test.py
