a2b3a0dde981230395107d03ff825fd2b7aa9f66
bipinupd <bipinupd@gmail.com>
2020-09-15 00:11:29 -0400

    Test

--- tests/it-test/generic_deidentify_template_test.py
+++ tests/it-test/generic_deidentify_template_test.py
@@ -1,5 +1,5 @@
 from pytest import mark
-from mytestutils import common
+import google.cloud.dlp
 
 @mark.smoke
 class GenericDeIdentifyDLPTest:
@@ -19,7 +19,7 @@ class GenericDeIdentifyDLPTest:
                 ]
             ]
         }
-        response = common.deindentify(data, project, self.template_id)
+        response = deindentify(data, project, self.template_id)
         assert response.item.table.rows[0].values[0].string_value != "test@test.com"
 
     def test_deIdentification_UserName(self, project):
@@ -33,7 +33,20 @@ class GenericDeIdentifyDLPTest:
                 ]
             ]
         }
-        response = common.deindentify(data, project, self.template_id)
+        response = deindentify(data, project, self.template_id)
         assert response.item.table.rows[0].values[0].string_value != "test_user"
         
-        
+    def deindentify(self, data, project_id, template_id):
+        headers = [{"name": val} for val in data["header"]]
+        rows = []
+        for row in data["rows"]:
+            rows.append( {"values": [{"string_value": cell_val} for cell_val in row]} )
+        table = {}
+        table["headers"] = headers
+        table["rows"] = rows
+        item = {"table": table}
+        dlp = google.cloud.dlp_v2.DlpServiceClient()
+        parent = dlp.project_path(project_id)
+        deidentify_template=f"projects/{project_id}/deidentifyTemplates/{template_id}"
+        response = dlp.deidentify_content(parent, deidentify_template_name=deidentify_template,item=item)
+        return response    
--- tests/it-test/inspect_template_test.py
+++ tests/it-test/inspect_template_test.py
@@ -1,4 +1,4 @@
-from mytestutils import common
+import google.cloud.dlp
 from pytest import mark
 
 @mark.smoke
@@ -10,15 +10,23 @@ class GenericInspectTest:
 
     def test_inspect_age(self, project):
         item = {'value': 'I am 25 years old'}
-        response = common.inspectdata(item, project,self.template_id)
+        response = inspectdata(item, project,self.template_id)
         assert response.result.findings[0].info_type.name == "AGE"
 
     def test_inspect_gender(self, project):
         item = {'value': 'The victim was male'}
-        response = common.inspectdata(item, project,self.template_id)
+        response = inspectdata(item, project,self.template_id)
         assert response.result.findings[0].info_type.name == "GENDER"
 
     def test_inspect_email(self, project):
         item = {'value': 'My email is test@example.com'}
-        response = common.inspectdata(item, project,self.template_id)
+        response = inspectdata(item, project,self.template_id)
         assert response.result.findings[0].info_type.name == "EMAIL_ADDRESS"
+    
+
+    def inspectdata(self, data, project_id, template_id):
+        dlp = google.cloud.dlp_v2.DlpServiceClient()
+        parent = dlp.project_path(project_id)
+        inspect_template=f"projects/{project_id}/inspectTemplates/{template_id}"
+        response = dlp.inspect_content(parent, inspect_template_name=inspect_template,item=data)
+        return response
