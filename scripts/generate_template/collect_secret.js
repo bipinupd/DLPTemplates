4dda5709c8eaff36d46dba964b07df580c0c7d04
bipinupd <bipinupd@gmail.com>
2020-09-15 15:40:43 -0400

    Refactor

--- scripts/generate_template/collect_secret.js
+++ scripts/generate_template/collect_secret.js
@@ -6,7 +6,6 @@ const options = yargs
  .usage("Usage: -p project")
  .option("p", { alias: "project", describe: "Project-Id", type: "string", demandOption: true })
  .option("d", { alias: "deidentify_templates_dir", describe: "DeIdentify Templates", type: "string", demandOption: true })
- .option("s", { alias: "secret", describe: "Command seperated secrets", type: "string", demandOption: true })
  .argv;
 
 const projectId = options.project;
@@ -14,7 +13,7 @@ const deidentifyTemplateDir = options.deidentify_templates_dir;
 const secretNames = options.secret.split(",");
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 const client = new SecretManagerServiceClient();
-var data={}
+var data={};
 
 // Reads the secret and update the data
 async function accessSecretVersion(secretName, data) {
@@ -26,12 +25,20 @@ async function accessSecretVersion(secretName, data) {
 }
 
 //Collect all KMS wrapperd keys from Secret manage required to register template
-const getAllWrapperdKeysFromSecretManager = async () => {
-    console.log(secretNames)
+const getValuesFromSecretManager = async (secretNames) => {
     for (secretName of secretNames) { 
       await accessSecretVersion(secretName, data);
     }
 }
+function getAllKeys(data) {
+    var keys=[];
+    var elements = JsonTemplate.select(data, function(key, val) {
+      return /^{{.*}}$/.test(val);
+    })
+    elements.values().forEach((element) => { keys.push(element.replace("{{","").replace("}}","").split(".")[0]);})
+    return keys;
+}
+
 //Repalce the value of KMS wrapped key and crypto key name in DLP templates 
 const generateDLPTemplates = async () => {
     await getAllWrapperdKeysFromSecretManager();
@@ -39,8 +46,10 @@ const generateDLPTemplates = async () => {
     let filenames = fs.readdirSync(deidentifyTemplateDir);
     const ST = require('selecttransform').SelectTransform;
     const st = new ST();
-    filenames.forEach((file) => {
+    filenames.forEach((file) => {        
         const template=fs.readFileSync(deidentifyTemplateDir + path.sep + file,"utf-8")
+        const keys = getAllKeys(JSON.paser(template));
+        await getValuesFromSecretManager(keys)
         const dlpTemplate = st.transformSync(template, data)
         fs.writeFile(outputDeidentifyDir + path.sep + file, dlpTemplate, function (err) {
         if (err) throw err;
