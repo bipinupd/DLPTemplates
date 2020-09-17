b633053178a1386f4b8677f20c6d0454891ffc75
Bipin Upadhyaya <bipinupd@gmail.com>
2020-09-16 22:08:53 -0400

    Test 06 (#9)
    * New Template

---
+++ tests/it-test/another_deidentification_example_test.py
@@ -0,0 +1,52 @@
+from pytest import mark
+import google.cloud.dlp
+
+@mark.smoke
+class AnotherDeIdentifyDLPTest:
+    
+    @classmethod 
+    def setup_class(self):
+        self.template_id = "another_deidentification_example"
+
+    def test_deIdentification_Email(self, project):
+        data = {
+            "header":[
+                "Email"
+            ],
+            "rows":[
+                [
+                    "test@test.com"
+                ]
+            ]
+        }
+        response = self.deindentify(data, project, self.template_id)
+        assert response.item.table.rows[0].values[0].string_value != "test@test.com"
+
+    def test_deIdentification_UserName(self, project):
+        data = {
+            "header":[
+                "UserName"
+            ],
+            "rows":[
+                [
+                    "test_user"
+                ]
+            ]
+        }
+        response = self.deindentify(data, project, self.template_id)
+        assert response.item.table.rows[0].values[0].string_value != "test_user"
+        
+    def deindentify(self, data, project_id, template_id):
+        headers = [{"name": val} for val in data["header"]]
+        rows = []
+        for row in data["rows"]:
+            rows.append( {"values": [{"string_value": cell_val} for cell_val in row]} )
+        table = {}
+        table["headers"] = headers
+        table["rows"] = rows
+        item = {"table": table}
+        dlp = google.cloud.dlp_v2.DlpServiceClient()
+        parent = dlp.project_path(project_id)
+        deidentify_template=f"projects/{project_id}/deidentifyTemplates/{template_id}"
+        response = dlp.deidentify_content(parent, deidentify_template_name=deidentify_template,item=item)
+        return response
