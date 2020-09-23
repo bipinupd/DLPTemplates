f9cd99ae903c5fb96659879b291ae256ad7369fb
bipinupd <bipinupd@gmail.com>
2020-09-22 21:55:23 -0400

    Deploying DLP templates

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p /workspace/package/DLPTemplates/{deIdentify_templates,inspect_templates}
-mkdir -p /workspace/package/DLPTemplates/tests/{utils,it-test}
+mkdir -p /workspace/package/DLPTemplates/tests/{utils,ittest}
 cd /workspace/DLPTemplates/
 cp -R scripts/ /workspace/package/DLPTemplates/scripts/
 cp tests/*.* /workspace/package/DLPTemplates/tests/
-touch /workspace/package/DLPTemplates/deIdentify_templates_to_delete
-touch /workspace/package/DLPTemplates/inspect_templates_to_delete
+touch /workspace/package/DLPTemplates/deIdentify_templates_to_delete.txt
+touch /workspace/package/DLPTemplates/inspect_templates_to_delete.txt
 input="/workspace/dlp-diff.txt"
 cd /workspace/DLPTemplates
 while IFS= read -r line
@@ -16,9 +16,9 @@ do
       cp "$line" "/workspace/package/DLPTemplates/deIdentify_templates/"
       template_file=`echo "${line}" | awk -F/ '{print $NF}'`
       it_test_file="${template_file%.*}"_test.py
-      cp "tests/it-test/$it_test_file" "/workspace/package/DLPTemplates/tests/it-test/"
+      cp "tests/ittest/$it_test_file" "/workspace/package/DLPTemplates/tests/ittest/"
     else
-      echo "$line" | cut -d '/' -f 2 | sed -e 's/.json//g' >> "/workspace/package/DLPTemplates/deIdentify_templates_to_delete"
+      echo "$line" | cut -d '/' -f 2 | sed -e 's/.json//g' >> "/workspace/package/DLPTemplates/deIdentify_templates_to_delete.txt"
     fi
   fi
   if [[ "$subfolder" == "inspect_templates" ]]; then
@@ -26,9 +26,9 @@ do
       cp "$line" "/workspace/package/DLPTemplates/inspect_templates/"
       template_file=`echo "${line}" | awk -F/ '{print $NF}'`
       it_test_file="${template_file%.*}"_test.py
-      cp "tests/it-test/$it_test_file" "/workspace/package/DLPTemplates/tests/it-test/"
+      cp "tests/ittest/$it_test_file" "/workspace/package/DLPTemplates/tests/ittest/"
     else
-      echo "$line" | cut -d '/' -f 2 | sed -e 's/.json//g' >> "/workspace/package/DLPTemplates/inspect_templates_to_delete"
+      echo "$line" | cut -d '/' -f 2 | sed -e 's/.json//g' >> "/workspace/package/DLPTemplates/inspect_templates_to_delete.txt"
     fi
   fi
 done < "$input"
--- tests/unittest/dlp_test.py
+++ tests/unittest/dlp_test.py
@@ -21,10 +21,8 @@ class BuildTest:
         json_pattern = os.path.join(directory, '*.json')
         file_list = glob.glob(json_pattern)
         for file in file_list:
-            print(file)
             with open(file) as file_loaded:
                 try:
-                    print(file_loaded)
                     json.load(file_loaded)
                 except:
                     pytest.fail("Invalid JSON for " + file)
@@ -32,15 +30,11 @@ class BuildTest:
     def check_template_name(self, directory):
         json_pattern = os.path.join(directory, '*.json')
         file_list = glob.glob(json_pattern)
-        template_name=[]
         for file in file_list:
-            print(file)
             with open(file) as file_loaded:
                 try:
-                    print(file_loaded)
                     data = json.load(file_loaded)
-                    if data["templateId"] in template_name:
-                        pytest.fail("Duplicate template_Id for " + file)
-                    template_name.append(data["templateId"])
+                    if (os.path.splitext(file)[0] != data["templateId"]):
+                        pytest.fail("File Name and template_Id does not match " + file)
                 except:
                     pytest.fail("Invalid JSON for " + file) 
